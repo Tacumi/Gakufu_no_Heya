90eb63ace7ef2b3cd768906e31fc6fa866793e8b
Tacumi <masa258333@gmail.com>
2015-01-14 20:35:31 +0900

    [add] add a music button to screen.java
    add a music button to screen class
    but it haven't implemented music player.

--- screen.java
+++ screen.java
@@ -11,9 +11,24 @@ public class screen{
 	}
 	public screen(){
 		int winWidth=600,winHeight=800;
+		JPanel musicPlayButton;
 		mainframe = new JFrame();
+		mainframe.setLayout(null);
+		musicPlayButton = new pushButton();
+		mainframe.add(musicPlayButton);
 		mainframe.setBounds(100,100,winWidth,winHeight);
+		musicPlayButton.setBounds(winWidth/4*3,winHeight-winHeight/5,winWidth/4,winHeight/5);
+		musicPlayButton.setBackground(Color.blue);
 		mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainframe.setVisible(true);
+	}
+}
+
+class pushButton extends JPanel{
+	public pushButton(){
+		super();
+	}
+	public void setBounds(int x, int y, int width, int height){
+		super.setBounds(x,y,width,height);
 	}
 }
