144d38aee2fe2cba29f4c3fcdd95968e21806194
Tacumi <masa258333@gmail.com>
2015-01-14 20:01:40 +0900

    [new] create a screen class
    create a blank screen class with height:800 width:600

--- screen.java
+++ screen.java
@@ -1,1 +1,17 @@
 package GakufunoHeya;
+
+import javax.swing.*;
+import GakufunoHeya.*;
+
+public class screen{
+	JFrame mainframe;
+	public static void main(String args[]){
+		new screen();
+	}
+	public screen(){
+		mainframe = new JFrame();
+		mainframe.setBounds(100,100,600,800);
+		mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainframe.setVisible(true);
+	}
+}
