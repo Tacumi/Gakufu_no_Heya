83373a155a7cb438479650f161536fd787d42525
Tacumi <masa258333@gmail.com>
2015-01-14 19:55:25 +0900

    [add] add new methods to music_play.java
    add start(), stop() and milisleep(int) to
    music_play.java

--- music_play.java
+++ music_play.java
@@ -13,4 +13,36 @@ public class music_play{
 		seq.close();
 		return;
 	}
+	public void start(){
+		if( midiFilename == null || seq == null )
+			return;
+		try {
+			seq.open();
+			seq.setSequence(MidiSystem.getSequence(
+						new File(this.midiFilename)));
+			seq.start();
+			while(seq.isRunning())milisleep(100);
+			seq.stop();
+			seq.close();
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	public void stop(){
+		if( midiFilename == null || seq == null )
+			return;
+		try {
+			seq.stop();
+			seq.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	public void milisleep(int miliseconds){
+		try {
+			Thread.currentThread().sleep(100);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 }
