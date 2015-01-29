7a53a868ef2b669a90df77e95f527dada412c2ad
K.Kitajima <k751c74dc@outlook.com>
2015-01-30 01:09:06 +0900

    [modify] change imput of measureList.java
    change imput String to byte

--- measureList.java
+++ measureList.java
@@ -2,60 +2,47 @@ package GakufunoHeya;
 
 public class measureList {
 
-	final byte amountPiano = 0;
-	final byte amountBase = 3;
-	final byte amountDrum = 8;
-	final static short bpm = 105;
+	final static byte amountBase = 4;
+	final static byte amountDrum = 8;
 
-	public static String pianoList(String str) {
-		switch (str) {
-		case "0":
-			return "./midi/piano0.mid";
-		case "1":
-			return "./midi/piano1.mid";
-		default:
-			System.err.println("Can't read this measure name!!");
-			System.err.println("imput piano name : " + str);
-			return "";
-		}
-	}
-
-	public static String baseList(String str) {
-		switch (str) {
-		case "0":
+	public static String baseList(byte i) {
+		switch (i) {
+		case 0:
 			return "./midi/base0.mid";
-		case "1":
+		case 1:
 			return "./midi/base1.mid";
-		case "2":
+		case 2:
 			return "./midi/base2.mid";
+		case 3:
+			return "./midi/base3.mid";
 		default:
 			System.err.println("Can't read this measure name!!");
-			System.err.println("imput base name : " + str);
+			System.err.println("imput base number : " + i);
 			return "";
 		}
 	}
 
-	public static String drumList(String str) {
-		switch (str) {
-		case "0":
+	public static String drumList(int i) {
+		switch (i) {
+		case 0:
 			return "./midi/drum0.mid";
-		case "1":
+		case 1:
 			return "./midi/drum1.mid";
-		case "2":
+		case 2:
 			return "./midi/drum2.mid";
-		case "3":
+		case 3:
 			return "./midi/drum3.mid";
-		case "4":
+		case 4:
 			return "./midi/drum4.mid";
-		case "5":
+		case 5:
 			return "./midi/drum5.mid";
-		case "6":
+		case 6:
 			return "./midi/drum6.mid";
-		case "7":
+		case 7:
 			return "./midi/drum7.mid";
 		default:
 			System.err.println("Can't read this measure name!!");
-			System.err.println("imput drum name : " + str);
+			System.err.println("imput drum number : " + i);
 			return "";
 		}
 	}
