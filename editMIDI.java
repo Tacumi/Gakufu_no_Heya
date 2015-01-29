78b47ca14693c2cd4ee9b4211a889732a88e5110
Tacumi <masa258333@gmail.com>
2015-01-29 15:54:45 +0900

    [delete] delete editMIDI

--- editMIDI.java
+++ editMIDI.java
@@ -4,29 +4,6 @@ public class editMIDI {
 	String midiFileName;
 	public editMIDI(String midiFileName) {
 		this.midiFileName = midiFileName;
-		measureList mList = new measureList();
-
-		String RamdomP = Integer
-				.toHexString((int) (Math.random() * mList.amountPiano));
-		String RamdomB = Integer
-				.toHexString((int) (Math.random() * mList.amountBase));
-		String RamdomD = Integer
-				.toHexString((int) (Math.random() * mList.amountDrum));
-
-		StringBuffer buffer = new StringBuffer();
-		buffer.append(sts.pianoMusic);
-		buffer.append(RamdomP);
-		sts.pianoMusic = buffer.toString();
-
-		buffer.setLength(0);
-		buffer.append(sts.baseMusic);
-		buffer.append(RamdomB);
-		sts.baseMusic = buffer.toString();
-
-		buffer.setLength(0);
-		buffer.append(sts.drumMusic);
-		buffer.append(RamdomD);
-		sts.drumMusic = buffer.toString();
 	}
 
 }
